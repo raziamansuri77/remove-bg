d63bc1f20cbd9b1b7947af3bf4b97925aa2d2cdb
razia <raziamansuri77@gmail.com>
2024-06-21 18:29:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const howToUse = document.getElementById("use");
+const howTow = document.getElementById("howTow");
+howToUse.addEventListener("click", () => {
+  // howTow.classList.remove("hidden");
+  alert("hello");
+});
